476ee2812da7b8a1071ccd306b60fa97be0345c4
Emerson <lopes.ds@live.com>
2024-06-23 12:00:35 -0300

    chore: removendo requisicao.

--- src/main/java/br/com/emerlopes/customerauthentication/application/entrypoint/rest/AuthenticationController.java
+++ src/main/java/br/com/emerlopes/customerauthentication/application/entrypoint/rest/AuthenticationController.java
@@ -1,18 +1,14 @@
 package br.com.emerlopes.customerauthentication.application.entrypoint.rest;
 
 import br.com.emerlopes.customerauthentication.application.entrypoint.rest.dto.AuthenticationRequestDTO;
-import br.com.emerlopes.customerauthentication.application.entrypoint.rest.dto.RegisterUserRequestDTO;
 import br.com.emerlopes.customerauthentication.application.entrypoint.rest.shared.CustomResponseDTO;
 import br.com.emerlopes.customerauthentication.application.exceptions.InvalidLoginException;
 import br.com.emerlopes.customerauthentication.application.mapper.AuthenticationDomainEntityMapper;
-import br.com.emerlopes.customerauthentication.application.mapper.UserDomainEntityMapper;
 import br.com.emerlopes.customerauthentication.domain.entity.AuthenticationDomainEntity;
 import br.com.emerlopes.customerauthentication.domain.usecase.AuthenticationUseCase;
 import br.com.emerlopes.customerauthentication.domain.usecase.GenerateAuthenticationUseCase;
-import br.com.emerlopes.customerauthentication.domain.usecase.RegisterUserUseCase;
 import br.com.emerlopes.customerauthentication.domain.usecase.ValidateTokenUseCase;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
 import lombok.SneakyThrows;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,18 +25,14 @@ public class AuthenticationController {
     private final ValidateTokenUseCase validateTokenUseCase;
 
 
-    final RegisterUserUseCase registerUserUseCase;
-
     public AuthenticationController(
             final GenerateAuthenticationUseCase generateAuthenticationUseCase,
             final AuthenticationUseCase authenticationUseCase,
-            final ValidateTokenUseCase validateTokenUseCase,
-            final RegisterUserUseCase registerUserUseCase
+            final ValidateTokenUseCase validateTokenUseCase
     ) {
         this.generateAuthenticationUseCase = generateAuthenticationUseCase;
         this.authenticationUseCase = authenticationUseCase;
         this.validateTokenUseCase = validateTokenUseCase;
-        this.registerUserUseCase = registerUserUseCase;
     }
 
     @PostMapping("/validate")
@@ -53,31 +45,6 @@ public class AuthenticationController {
 
         return ResponseEntity.status(HttpStatus.OK).body(
                 new CustomResponseDTO<AuthenticationDomainEntity>().setData(login));
-    }
-
-    @PostMapping
-    public ResponseEntity<?> generateToken(
-            final @Valid @RequestPart("email") @Email String email
-    ) {
-
-        final var authenticationDomainEntity = AuthenticationDomainEntity.builder()
-                .username(email)
-                .build();
-
-        final var executionResult = generateAuthenticationUseCase.execute(authenticationDomainEntity);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(
-                new CustomResponseDTO<AuthenticationDomainEntity>().setData(executionResult));
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity<?> registerUser(
-            final @RequestBody @Valid RegisterUserRequestDTO request
-    ) {
-        final var userDomainEntity = UserDomainEntityMapper.toDomainEntity(request);
-        registerUserUseCase.execute(userDomainEntity);
-
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @SneakyThrows
