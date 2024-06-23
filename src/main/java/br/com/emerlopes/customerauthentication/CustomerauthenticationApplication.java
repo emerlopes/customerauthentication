231f66d2ff31cd83ad7d568acc715bf3b63a8553
Emerson <lopes.ds@live.com>
2024-06-22 21:24:25 -0300

    chore: initial commit

---
+++ src/main/java/br/com/emerlopes/customerauthentication/CustomerauthenticationApplication.java
@@ -0,0 +1,13 @@
+package br.com.emerlopes.customerauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerauthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerauthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/emerlopes/customerauthentication/CustomerauthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emerlopes.customerauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerauthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
