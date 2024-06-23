de2c4f9b2050fda53600576cd45465e03b8581e9
Emerson <lopes.ds@live.com>
2024-06-23 12:01:57 -0300

    chore: implementando requisicoes para criar usuários.

--- src/main/java/br/com/emerlopes/customerauthentication/application/entrypoint/rest/UserController.java
+++ src/main/java/br/com/emerlopes/customerauthentication/application/entrypoint/rest/UserController.java
@@ -1,14 +1,16 @@
 package br.com.emerlopes.customerauthentication.application.entrypoint.rest;
 
+import br.com.emerlopes.customerauthentication.application.entrypoint.rest.dto.RegisterUserRequestDTO;
 import br.com.emerlopes.customerauthentication.application.entrypoint.rest.shared.CustomResponseDTO;
+import br.com.emerlopes.customerauthentication.application.mapper.UserDomainEntityMapper;
 import br.com.emerlopes.customerauthentication.domain.entity.UserDomainEntity;
 import br.com.emerlopes.customerauthentication.domain.usecase.FindUsersUseCase;
+import br.com.emerlopes.customerauthentication.domain.usecase.RegisterAdminUseCase;
+import br.com.emerlopes.customerauthentication.domain.usecase.RegisterGuestUseCase;
 import br.com.emerlopes.customerauthentication.domain.usecase.RegisterUserUseCase;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,14 +19,50 @@ import java.util.List;
 public class UserController {
 
     final RegisterUserUseCase registerUserUseCase;
+    final RegisterGuestUseCase registerGuestUseCase;
+    final RegisterAdminUseCase registerAdminUseCase;
     final FindUsersUseCase findUsersUseCase;
 
     public UserController(
             final RegisterUserUseCase registerUserUseCase,
+            final RegisterGuestUseCase registerGuestUseCase,
+            final RegisterAdminUseCase registerAdminUseCase,
             final FindUsersUseCase findUsersUseCase
     ) {
         this.registerUserUseCase = registerUserUseCase;
+        this.registerGuestUseCase = registerGuestUseCase;
+        this.registerAdminUseCase = registerAdminUseCase;
         this.findUsersUseCase = findUsersUseCase;
+    }
+
+    @PostMapping("/register-guest")
+    public ResponseEntity<?> registerGuest(
+            final @RequestBody RegisterUserRequestDTO request
+    ) {
+        final var userDomainEntity = UserDomainEntityMapper.toDomainEntity(request);
+        registerGuestUseCase.execute(userDomainEntity);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PostMapping("/register-user")
+    public ResponseEntity<?> registerUser(
+            final @RequestBody RegisterUserRequestDTO request
+    ) {
+        final var userDomainEntity = UserDomainEntityMapper.toDomainEntity(request);
+        registerUserUseCase.execute(userDomainEntity);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PostMapping("/register-admin")
+    public ResponseEntity<?> registerAdmin(
+            final @RequestBody RegisterUserRequestDTO request
+    ) {
+        final var userDomainEntity = UserDomainEntityMapper.toDomainEntity(request);
+        registerAdminUseCase.execute(userDomainEntity);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @GetMapping
