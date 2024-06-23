ebbdeb99d6b60b2dad1c3461f62993e7ede29681
Emerson <lopes.ds@live.com>
2024-06-23 12:02:58 -0300

    chore: atualizando política de acesso.

--- src/main/java/br/com/emerlopes/customerauthentication/infrastructure/security/SecurityConfig.java
+++ src/main/java/br/com/emerlopes/customerauthentication/infrastructure/security/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users/register*").permitAll()
                         .requestMatchers(HttpMethod.GET, "/users").hasRole(UserRole.ADMIN.name())
                         .anyRequest().authenticated()
                 )
