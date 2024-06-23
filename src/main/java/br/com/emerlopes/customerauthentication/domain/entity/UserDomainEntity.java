e51f4247b90c67d1020f3b76f88a9e1baaf9c062
Emerson <lopes.ds@live.com>
2024-06-23 17:43:07 -0300

    chore: adicionando anotacao

--- src/main/java/br/com/emerlopes/customerauthentication/domain/entity/UserDomainEntity.java
+++ src/main/java/br/com/emerlopes/customerauthentication/domain/entity/UserDomainEntity.java
@@ -1,6 +1,7 @@
 package br.com.emerlopes.customerauthentication.domain.entity;
 
 import br.com.emerlopes.customerauthentication.domain.shared.UserRole;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -8,6 +9,7 @@ import lombok.experimental.Accessors;
 @Data
 @Builder
 @Accessors(chain = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDomainEntity {
     private String id;
     private String login;
